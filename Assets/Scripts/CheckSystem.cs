13e73baa0d25b820b0182af838beb1d9b10fe052
DAVID NYCOLAS GOMES ASSIS <49223463840@info.local>
2025-02-03 17:30:46 -0300

    feat: script morte

--- Assets/Scripts/CheckSystem.cs
+++ Assets/Scripts/CheckSystem.cs
@@ -7,7 +7,7 @@ public class CheckSystem : MonoBehaviour
 
     public List<GameObject> CheckPoints;
 
-    public Vector3 VectorPoint;
+    public Vector3 SpawnPoint;
 
     public float EixoLimite;
 
@@ -17,19 +17,29 @@ public class CheckSystem : MonoBehaviour
     {
         if (player.transform.position.y < EixoLimite || Input.GetKeyDown(KeyCode.E))
         {
-            player.transform.position = VectorPoint;
-            player.transform.eulerAngles = VectorPoint;
+            player.transform.position = SpawnPoint;
+            player.transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+            
         }
     }
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Piso"))
         {
 
-            VectorPoint = other.transform.position;
+            SpawnPoint = other.transform.position;
             Destroy(other.gameObject);
         }
+
+        if (other.gameObject.CompareTag("matador"))
+        {
+            transform.position = (SpawnPoint);
+              
+        }
+
     }
 
 }
